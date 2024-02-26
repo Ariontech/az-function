a705925d711f7d22e9ce79328b968b8afeaf1e74
MustaphaMouelhi <m.mouelhi@arion-tech.com>
2024-02-26 17:24:46 +0100

    test PR

--- az-function-1/__init__.py
+++ az-function-1/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 import azure.functions as func
-
+import math
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
